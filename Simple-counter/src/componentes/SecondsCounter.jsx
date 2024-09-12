265fc25422710396c43c313204093685bb838313
ElioPeraza <Perazaelio@gmail.com>
2024-09-12 12:40:56 -0300

    last update

--- Simple-counter/src/componentes/SecondsCounter.jsx
+++ Simple-counter/src/componentes/SecondsCounter.jsx
@@ -5,30 +5,35 @@ import { useState } from 'react'
 
 const SecondsCounter = () => {
   const [SecondsCounter, setSecondsCounter] = useState(0)
+ 
 
 
   useEffect(() => {
     const interval = setInterval(() => {
       setSecondsCounter ( reset =>
-        reset >= 3434 ? 0 : reset + 1 
+        reset >= 3434 ? 0 : reset + 1
       );
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [SecondsCounter])
+  }, [SecondsCounter, ])
 
 
 
 
-  const dividir= String(SecondsCounter).padStart(6, '0'); // Aseguramos que el contador tenga 6 dígitos
+  const dividir= String(SecondsCounter).padStart(6, '0'); 
 
+  
   return (
+    <div>
     <h1>
       <i className="fa-solid fa-clock"></i>
       {dividir.split('').map((digit) => (
         <span className="counter-digit">{digit}</span>
       ))}
     </h1>
+    
+    </div>
   );
 };
 
